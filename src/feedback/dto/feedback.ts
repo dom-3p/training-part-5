27fde2e4de06649362ca9bd461e8cb83de2e867c
Dom Moreitz <dom.moreitz@3plearning.com>
2022-04-01 12:01:49 +1100

    Added feedback DTO attributes for swagger

--- src/feedback/dto/feedback.ts
+++ src/feedback/dto/feedback.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Feedback {
+  @ApiProperty({ description: 'The feedback category' })
   category: string;
+
+  @ApiProperty({
+    description: 'A flag to show if this feedback item has been actioned',
+  })
   actioned: boolean;
+
+  @ApiProperty({ description: 'When this feedback item was created' })
+  @ApiProperty()
   createdAt: Date;
+
+  @ApiProperty({
+    description: 'The unique content id to which this feedback relates',
+  })
+  @ApiProperty()
   contentId: string; // GUID / UUID format
+
+  @ApiProperty({ description: 'The id of the user lodging this feedback' })
   userId: number;
 }
